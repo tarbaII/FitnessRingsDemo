c2a85eaa2ae53a1d23f890ae253129ff9e6ff24c
Alyx Ferrari <rubiquikyt@gmail.com>
2022-04-27 10:59:13 -0400

    Initial Commit

---
+++ FitnessRingsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessRingsDemo
+//
+//  Created by Alyx Ferrari on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessRingsDemo/FitnessRingsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessRingsDemoApp.swift
+//  FitnessRingsDemo
+//
+//  Created by Alyx Ferrari on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessRingsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
